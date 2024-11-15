4fc06ba8143ad34bb72b61bfbf27da4f5c248043
Rudra130 <rt1766285@gmail.com>
2024-11-16 01:26:55 +0530

    use states

--- React/project/src/App.jsx
+++ React/project/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
+
+
 
 function App() {
+  let [counter ,setState] = useState(0)
+//let counter=15
+
+const addValue = ()=>{
+  if(counter>20){
+    setState(0)
+    alert("Counter is already at max")
+  }else{
+  setState(counter+1)
+  }
+}
+
+const removeValue =() =>{
+  if(counter===0){
+    alert("Counter is already at min")
+  }else{
+    setState(counter-1)
+  }
+}
   return (
-    <h1>Chai aur react with vite</h1>
+    <>   
+    <h1>Counter </h1>
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br/>
+     <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
   )
 
  
